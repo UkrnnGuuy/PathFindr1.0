3d41d4e4ba7db3a84028f9e134fea48c95bbcef9
UkrnnGuuy <Vladyslav.Zakharov@stu.ocsb.ca>
2025-04-07 09:12:21 -0400

    Update PCU.py

--- PCU.py
+++ PCU.py
@@ -1,4 +1,27 @@
-import numpy
-import noise
-import mathplot
-#Hoping this works!
+#from perlin_noise import PerlinNoise
+import numpy as np
+import matplotlib
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+
+noise1 = PerlinNoise(octaves=3)
+noise2 = PerlinNoise(octaves=6)
+noise3 = PerlinNoise(octaves=12)
+noise4 = PerlinNoise(octaves=24)
+
+xpix, ypix = 100, 100
+pic = []
+for i in range(xpix):
+    row = []
+    for j in range(ypix):
+        noise_val = noise1([i/xpix, j/ypix])
+        noise_val += 0.5 * noise2([i/xpix, j/ypix])
+        noise_val += 0.25 * noise3([i/xpix, j/ypix])
+        noise_val += 0.125 * noise4([i/xpix, j/ypix])
+
+        row.append(noise_val)
+    pic.append(row)
+
+plt.imshow(pic, cmap='gray')
+plt.show()
